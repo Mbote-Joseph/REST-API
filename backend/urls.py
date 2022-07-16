5f4c12f24e9f120a553cd7dfcd0004511b5df86d
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-16 21:14:25 +0300

    REST API Project

--- backend/urls.py
+++ backend/urls.py
@@ -12,3 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    
+]
