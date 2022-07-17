31bf2c74c48f2d933aaa37bd05cd115cac81a810
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-17 22:26:39 +0300

    REST API Project

--- client/views.py
+++ client/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import api_view
+from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view(['GET'])
