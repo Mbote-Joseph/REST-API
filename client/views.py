551ae1e47135db2e57d91a718a47383694d69364
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-17 22:22:45 +0300

    REST API Project

--- client/views.py
+++ client/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def index(request):
+    return Response({'message': 'Hello, World!'})
+    
