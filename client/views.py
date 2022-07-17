fd48935f3425d970ad4e281fdc9b508ba857230f
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-17 22:30:20 +0300

    REST API Project

--- client/views.py
+++ client/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from . import UserSerializer
+
 # Create your views here.
 @api_view(['GET'])
 def get(request):
-    return Response({'message': 'Hello, World!'})
+    users = UserSerializer.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+    
