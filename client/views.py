45ab0cf0db8ec2ed4c46402f95a4fa3a574b7606
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-18 11:55:29 +0300

    REST API Project

--- client/views.py
+++ client/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from client.models import User
+from .models import User
 
 
 from client.serializers import UserSerializers
@@ -9,7 +9,7 @@ from client.serializers import UserSerializers
 # Create your views here.
 @api_view(['GET'])
 def getUser(request):
-    users = UserSerializers.objects.all()
+    users = User.objects.all()
     serializer = UserSerializers(users, many=True)
     return Response(serializer.data)
     
--- restAPI/urls.py
+++ restAPI/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('client.urls')),
+    path('client/', include('client.urls')),
     path('', include('backend.urls')),
 ]
